4d7279a1d27753f0f76f3dffa9ea36d58f404305
byzz <eyedroot@gmail.com>
2024-03-25 16:21:00 +0900

    ThemeProvider

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { Appearance } from 'react-native-appearance';
+import { Appearance } from 'react-native';
 
 interface ThemeContextType {
   isDark: boolean;
