0a98bd0b8baa7d9fa4063be47113b80cb51aad82
byzz <eyedroot@gmail.com>
2024-03-25 16:01:13 +0900

    ThemeContext.tsx

---
+++ contexts/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { Appearance } from 'react-native-appearance';
+
+interface ThemeContextType {
+  isDark: boolean;
+  setScheme: (scheme: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  isDark: false,
+  setScheme: () => {},
+});
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [isDark, setIsDark] = useState(Appearance.getColorScheme() === 'dark');
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setIsDark(colorScheme === 'dark');
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  const setScheme = (scheme: string) => setIsDark(scheme === 'dark');
+
+  return (
+    <ThemeContext.Provider value={{ isDark, setScheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
